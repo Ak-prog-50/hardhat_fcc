0d977bd2b485f3171b070275ffcc3d8e6e00e71c
Ak <akalankapathirage22@gmail.com>
2022-05-08 14:03:35 +0530

    get the main net price feed addr if in mainnet + added fork url to config

--- hardhat.config.js
+++ hardhat.config.js
@@ -45,6 +45,16 @@ module.exports = {
     ],
   },
   networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+      }
+    },
+    localhost: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+      }
+    },
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
--- utils/helpers.js
+++ utils/helpers.js
@@ -6,6 +6,7 @@ const mockV3Args = {
     initialAnswer : 200000000000 //2000 usd with 8 decimals
 }
 const priceFeedAddr = "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e" // Rinkeby Price Feed
+const priceFeedMainnet = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419" // Mainnet Price Feed
 
 const deployMocks = async() => {
     const MockV3 = await ethers.getContractFactory("MockV3Aggregator")
@@ -17,9 +18,13 @@ const deployMocks = async() => {
 
 
 const getPriceFeedAddr = async() => {
-    if (local_development.includes(network.name)) {
+    if (local_development.includes(network.name) && !network.config.forking) {
         console.log("\tIn a Local Network :", network.name)
         return await deployMocks()
+    }
+    if (network.config.forking) {
+        console.log("\tIn the Mainnet Fork :", network.config.forking.url)
+        return ethers.utils.getAddress(priceFeedMainnet)
     }
     console.log("\tIn a remote Network :", network.name)
     return ethers.utils.getAddress(priceFeedAddr)
