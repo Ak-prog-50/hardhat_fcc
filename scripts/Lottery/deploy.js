338d0031fe2dfdb1b08321651d5a30a1b964064a
Ak <akalankapathirage22@gmail.com>
2022-05-09 10:18:33 +0530

    deploy script for lottery

---
+++ scripts/Lottery/deploy.js
@@ -0,0 +1,17 @@
+const { ethers } = require("hardhat")
+const { getPriceFeedAddr } = require("../../utils/helpers")
+
+const main = async () => {
+    const Lottery = await ethers.getContractFactory("Lottery");
+    const lottery = await Lottery.deploy(getPriceFeedAddr());
+    await lottery.deployed();
+    console.log("Lottery deployed to:", lottery.address);
+    return lottery
+};
+
+main().catch((error) => {
+    console.error(`	Error occured when deploying : ${error}`);
+    process.exitCode = 1;
+});
+
+module.exports = main;
