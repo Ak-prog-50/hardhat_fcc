5579f545a2343fffe4a5fe5959f19435f30ace06
Ak <akalankapathirage22@gmail.com>
2022-05-13 18:56:28 +0530

    Aggregator v3 interface version upgrade + addressToAmountDeposited equal instead of plus equal + endLottery() function + fulfillRandomWords function for vrf callback

--- contracts/Lottery/Lottery.sol
+++ contracts/Lottery/Lottery.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.6.0;
+pragma solidity >=0.6.0 <0.9.0;
 
-import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "hardhat/console.sol";
 
 contract Lottery is VRFConsumerBaseV2 {
-    // using SafeMathChainlink for uint256; //notes: Using safe math is only for uints. Need to use a library like abdk before pushing to production to check for overflow errros. (divi func in ABDK)
+    // using SafeMathChainlink for uint256; //! Using safe math is only for uints. Need to use a library like abdk before pushing to production to check for overflow errros. (divi func in ABDK)
 
     address public owner;
     address[] public participants;
-    uint8 public entranceFeeInUsd;
+    address payable public recentWinner;
+    int8 public entranceFeeInUsd;
     uint256 public requestId;
     uint64 subscriptionId;
     VRFCoordinatorV2Interface COORDINATOR; // default visibility is internal in vars.
@@ -34,13 +35,18 @@ contract Lottery is VRFConsumerBaseV2 {
     AggregatorV3Interface internal priceFeed;
     mapping (address => uint256) internal addressToAmountDeposited;
 
-    constructor(address _priceFeed, address _vrfCoordinator, uint8 _entranceFeeInUsd, uint64 _subscriptionId) public VRFConsumerBaseV2(_vrfCoordinator) {
-        COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
+    constructor(
+        address _priceFeed, 
+        address _vrfCoordinator, 
+        int8 _entranceFeeInUsd, 
+        uint64 _subscriptionId    
+    ) VRFConsumerBaseV2(_vrfCoordinator) {
         owner = msg.sender;
         entranceFeeInUsd = _entranceFeeInUsd;
         priceFeed = AggregatorV3Interface(_priceFeed);
         lotteryState = LotteryState.CLOSED;
         subscriptionId = _subscriptionId;
+        COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
     }
 
     // onlyOwner modifier
@@ -79,7 +85,7 @@ contract Lottery is VRFConsumerBaseV2 {
         require(msg.value >= getEntranceFee(), "You have to deposit at least 50 USD");
         
         participants.push(msg.sender);
-        addressToAmountDeposited[msg.sender] += msg.value;
+        addressToAmountDeposited[msg.sender] = msg.value;  //! check how this works when funds added twice by the same address from the second start of lottery.
     }
 
     function startLottery() external onlyOwner {
@@ -88,8 +94,38 @@ contract Lottery is VRFConsumerBaseV2 {
     }
 
     function endLottery() external onlyOwner checkOpened{
-        lotteryState = LotteryState.SELECTING_WINNER;
+        require(participants.length > 0, "No participants");
 
+        lotteryState = LotteryState.SELECTING_WINNER;
+        requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+    }
+
+    function fulfillRandomWords (uint256, uint256[] calldata _randomWords) internal override {
+        // require statements
+        require(lotteryState == LotteryState.SELECTING_WINNER, "Lottery is not in the SELECTING_WINNER state");
+        require(_randomWords.length > 0, "No random values");
+        require(participants.length > 0, "No participants");
+
+        uint256 indexOfWinner = _randomWords[0] % participants.length;
+        require(indexOfWinner < participants.length, "Index out of bounds");
+        
+        recentWinner = payable(participants[indexOfWinner]); // participants array is not payable.
+        require(recentWinner != address(0), "Funds Are going to Hell!");  //this check is not neccary i think.
+        recentWinner.transfer(address(this).balance);
+        
+        // reset particiapants array
+        participants = new address[](0);
+        
+        lotteryState = LotteryState.CLOSED;
     }
 
 }
+
+// test resting addresstoamount
+// test how to store previous data
