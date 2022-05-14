fc85bb406d6354f0f06107a76de3ddd3ee0c5b23
Ak <akalankapathirage22@gmail.com>
2022-05-14 20:39:03 +0530

    neccesary changes to deloy mocks functions

--- utils/helpers.js
+++ utils/helpers.js
@@ -8,7 +8,8 @@ const mockV3Args = {
 const priceFeedAddr = "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e" // Rinkeby Price Feed
 const priceFeedMainnet = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419" // Mainnet Price Feed
 const coordinatorAddr = "0x6168499c0cFfCaCD319c818142124B7A15E857ab" // Rinkeby Coordinator
-const linkTokenAddr = "0x01BE23585060835E02B77ef475b0Cc51aA1e0709"
+const linkTokenAddr = "0x01BE23585060835E02B77ef475b0Cc51aA1e0709" // Rinkeby Link Token
+const linkEthFeed = "0xFABe80711F3ea886C3AC102c81ffC9825E16162E" // Rinkeby Link ETH Feed
 
 const deployMockPriceFeed = async() => {
     const MockV3 = await ethers.getContractFactory("MockV3Aggregator")
@@ -19,19 +20,11 @@ const deployMockPriceFeed = async() => {
 }
 
 const deployMockCoordinator = async() => {
-    const Coordinator = await ethers.getContractFactory("VRFCoordinatorV2Mock")
-    const coordinator = await Coordinator.deploy(10000, 100)
+    const Coordinator = await ethers.getContractFactory("VRFCoordinatorV2TestHelper")
+    const coordinator = await Coordinator.deploy(linkTokenAddr, ethers.constants.AddressZero, linkEthFeed)
     await coordinator.deployed()
     console.log("Coordinator deployed to:", coordinator.address);
     return coordinator.address
-}
-
-const deployMockLink = async() => {
-    const MockLink = await ethers.getContractFactory("LinkToken")
-    const mockLink = await MockLink.deploy()
-    await mockLink.deployed()
-    console.log("MockLink deployed to:", mockLink.address);
-    return mockLink.address
 }
 
 
@@ -58,11 +51,7 @@ const getCoordinatorAddr = async() => {
 }
 
 const getLinkAddr = async() => {
-    if (local_development.includes(network.name) && !network.config.forking) {
-        console.log("\tIn a Local Network :", network.name)
-        return await deployMockLink()
-    }
-    console.log("\tIn a remote Network :", network.name)
+    console.log("\tCurrent Network is :", network.name)
     return ethers.utils.getAddress(linkTokenAddr)
 }
 
